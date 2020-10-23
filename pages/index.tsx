7e6c6c17b7fcbdcf34ac3d685374f3bab39397fd
KitaharaMugiro <you@example.com>
2020-10-24 01:53:19 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Button from '@material-ui/core/Button';
+
+export default () => {
+
+  return (
+    <div className="example">
+      <Button variant="contained" color="primary">
+        Hello World
+    </Button>
+    </div>
+  )
+}
